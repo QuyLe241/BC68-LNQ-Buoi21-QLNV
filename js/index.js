2a329104efdf2d26d078a159628df2ba13c9037d
QUY666666 <lenhatquy2401@gmail.com>
2024-06-19 13:39:42 +0700

    2

--- js/index.js
+++ js/index.js
@@ -24,38 +24,62 @@ document.getElementById("formQLNV").onsubmit = function (event) {
 
     // thêm nhân viên vào arr
     arrNhanVien.push(nhanVien);
+    renderArrNhanVien();
     // xóa dữ liệu sau khi người dùng nhập dữ liệu xong
     document.getElementById("formQLNV").reset();
     console.log(arrNhanVien);
+    // document.getElementById("tableDanhSach").innerHTML = "11111";
 }
 
 //      Hiển thị nội dung lên giao diện
 // let content = ""; 
-function renderArrNhanVien (arr = arrNhanVien) {
+function renderArrNhanVien () {
     // Tạo vòng lặp duyệt NV trong mảng For of
     let content = "";       // chứa các giá trị hiển thị ra thẻ html
-    for (let nhanVien of arr){
+    for (let nhanVien of arrNhanVien){
+        // let newArrNhanVien = new nhanVien();
+        // Object.assign(newArrNhanVien,nhanVien);
         // destructuring
         // lấy dữ liệu từ nhanVien
-        let {tknv, name, email, datepicker, chucvu} = arrNhanVien;
+
+        let {tknv, name, email, datepicker, chucvu} = nhanVien;
+
+        // <td>${nhanVien.tongLuong()}</td>
+        // <td>${nhanVien.xepLoai()}</td>
         content += `
-                <tr>
-                    <td>${tknv}</td>
-                    <td>${name}</td>
-                    <td>${email}</td>
-                    <td>${datepicker}</td>
-                    <td>${chucvu}</td>
-                    <td>${arrNhanVien.tongLuong()}</td>
-                    <td>${arrNhanVien.xepLoai()}</td>
-                    <td>
-                    <button onclick="deleteSinhVien('${txtMaSV}')" class="btn btn-danger">Xoá</button>
-                    </td>
-                  </tr>   
+        <tr>
+            <td>${tknv}</td>
+            <td>${name}</td>
+            <td>${email}</td>
+            <td>${datepicker}</td>
+            <td>${chucvu}</td>
+            <td></td>
+            <td></td>
+            <td>
+            <button onclick="" class="btn btn-danger">Xoá</button>
+            <button
+                    class="btn btn-warning"
+                    id="btnThem"
+                    data-toggle="modal"
+                    data-target="#myModal"
+                  >
+                    Sửa
+                  </button>
+            </td>
+        </tr>   
         `;
+        
     } 
 
     //  DOM tới tbody và hiển thị
+    //     document.getElementById("btnThemNV").onclick = function () {
+    //     document.getElementById("tableDanhSach").innerHTML = content;
+    // }
     document.getElementById("tableDanhSach").innerHTML = content;
 }
+renderArrNhanVien();
 // document.getElementById("tableDanhSach").innerHTML = content;
-
+// document.getElementById("tableDanhSach").innerHTML = "11111qeqeqeqeq111111111111";
+    // document.getElementById("btnThemNV").onclick = function () {
+    //     document.getElementById("tableDanhSach").innerHTML = "11111qeqeqeqeq111111111111";
+    // }
